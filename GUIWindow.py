151b96d654b50ba517c1d3eb538e0e8f44b4961c
akirzeder <104333863+akirzeder@users.noreply.github.com>
2022-06-02 14:31:57 -0500

    Initial GUIWindow Setup

---
+++ GUIWindow.py
@@ -0,0 +1,10 @@
+# Importing all Modules
+import tkinter as tk
+
+# Create a class for creating the basic GUI Window
+class GUIWindow():
+    # Create the window named "master_window"
+    master_window = tk.TK()
+    master_window.pack()
+
+    master_window.mainloop()
