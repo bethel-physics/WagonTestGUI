be22ecb1af2b63fb8f6f124b96243ae415b0ff90
Garrett Schindler <garrettschindler17@gmail.com>
2022-06-06 12:01:36 -0500

    TestSummary Table Changes

--- PythonFiles/TestSummary.py
+++ PythonFiles/TestSummary.py
@@ -4,11 +4,24 @@ class TestSummary(tk.Frame):
     def __init__(self, parent, master_window):
         super().__init__(master_window, width=700, height=500, background='coral')
 
+        # Adds the title to the TestSummary Frame
+        self.title = tk.Entry(self,  width=13, fg='#0d0d0d',
+                               font=('Arial',18,'bold'))
+        self.title.insert(tk.END, "Test Summary Page")
+        self.title.grid(row= 0, column= 1)
+        
+        
+        # Creates the "table" as a frame object
         self.table = tk.Frame(self)
 
+        # Creating the y - labels
         self.y_labels = []
-        self.x_labels = ["Speed", "Acceleration", "Resistance"]
 
+        # Creating the x - labels
+        self.x_labels = ["serial_num", "tester", "label_applied", "database_entry", "label_legibility", "power_cycle", "comments" ]
+
+
+        # Determines the sizes of the rows and columns
         self.total_rows = len(self.y_labels) + 1
         self.total_columns = len(self.x_labels)
         
@@ -16,7 +29,7 @@ class TestSummary(tk.Frame):
 
         # Add all of the entries from the database into the table
         for entry in self.get_entries_from_DB():
-            self.add_entry_to_table(entry[0], entry[1], entry[2], entry[3])
+            self.add_entry_to_table(entry[0], entry[1], entry[2], entry[3], entry[4], entry[5], entry[6])
         
 
 
@@ -25,29 +38,29 @@ class TestSummary(tk.Frame):
         # Horizontal Labels
         for index in range(self.total_columns):
             self._entry= tk.Entry(self.table, width=13, fg='blue',
-                               font=('Arial',16,'bold'))
-            self._entry.grid(row=0, column=index + 1)
+                               font=('Arial',12))
+            self._entry.grid(row=0, column=index)
             self._entry.insert(tk.END, self.x_labels[index])
 
 
-        self.add_entry_to_table("Garrett's Entry", 100, 200, "30 Ohms")
-        self.add_entry_to_table("Andrew's Entry", 150, 200, "764 Ohms")
+        # Adding dummy values to the DataBase
 
-        self.table.grid()
+        self.table.grid(row = 1, column= 1)
 
 
+        # Ensures the frame is the correct size
         self.grid_propagate(0)
 
 
     
-    def add_entry_to_table(self, _title, _speed, _acceleration, _resistance):
+    def add_entry_to_table(self, serial_num, tester, label_applied, database_entry, label_legibility, power_cycle, comments):
         
-        list = [_title, _speed, _acceleration, _resistance]
+        list = [serial_num, tester, label_applied, database_entry, label_legibility, power_cycle, comments]
         
-        for index in range(self.total_columns + 1):
+        for index in range(self.total_columns):
             self._entry= tk.Entry(self.table, width=13, fg='blue',
-                               font=('Arial',16,'bold'))
-            self._entry.grid(row=self.total_rows + 1, column=index)
+                               font=('Arial',12))
+            self._entry.grid(row=self.total_rows, column=index)
             self._entry.insert(tk.END, list[index])
 
         self.total_rows = self.total_rows + 1
@@ -56,5 +69,5 @@ class TestSummary(tk.Frame):
     def get_entries_from_DB(self):
         #TODO Implement this method with the Database
         
-        return [["Sample_Test", "10 m/s", "15.2 m/s^2", "45 Ohms"]]
+        return [["0100256", "Garrett Schindler", "0123", "Row=4, Col=180", "Good", "Good", "NONE" ]]
 
