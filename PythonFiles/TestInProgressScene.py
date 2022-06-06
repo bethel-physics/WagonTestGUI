7c52afbba129f9fb2fe63b7e63a2dc535831e1f5
akirzeder <104333863+akirzeder@users.noreply.github.com>
2022-06-06 15:25:32 -0500

    Updated TestInProgress Graphics

--- PythonFiles/TestInProgressScene.py
+++ PythonFiles/TestInProgressScene.py
@@ -8,24 +8,37 @@ class TestInProgressScene(tk.Frame):
 
     def __init__(self, parent, master_window, next_frame):
         self.next_frame = next_frame
-        super().__init__(master_window, width=700, height=500, background='purple')
+        super().__init__(master_window, width = 850, height = 500)
 
         # Creating grid frame to lay widgets onto
-        frm_main = tk.Frame(self, width = 200, height = 200)
-        frm_main.grid(column=0, row=0)
+        frm_main = tk.Frame(self, width = 425, height = 500)
+        frm_main.grid(column=0, row=0, sticky = 'nsew')
+        frm_main.grid_propagate(0)
 
         # Creating the main title in the frame
-        lbl_title = tk.Label(frm_main, text = "Test in progress. Please wait")
-        lbl_title.pack(padx=200, pady=200)
+        lbl_title = tk.Label(frm_main, text = "Test in progress. Please wait.", font = ('Arial', 20))
+        lbl_title.pack(padx = 0, pady = 100)
 
         # Create a progress bar of some kind
-        prgbar_progress = ttk.Progressbar(frm_main, orient = 'horizontal', mode = 'indeterminate', length = 280)
-        prgbar_progress.pack()
+        prgbar_progress = ttk.Progressbar(
+            frm_main, 
+            orient = 'horizontal', 
+            mode = 'indeterminate', length = 350)
+        prgbar_progress.pack(padx = 50)
         prgbar_progress.start()
 
         # A Button To Stop the Progress Bar and Progress Forward (Temporary until we link to actual progress)
         btn_stop = ttk.Button(frm_main, text='Stop', command= lambda: self.stop_button_action(parent, self.next_frame))
-        btn_stop.pack()
+        btn_stop.pack(padx = 0, pady = 125)
+
+        # Creating a Frame For Console Output
+        frm_console = tk.Frame(self, width = 425, height = 500, bg = 'black')
+        frm_console.grid_propagate(0)
+        frm_console.grid(column = 1, row = 0, sticky = 'nsew')
+
+        # Labeling frm_console
+        lbl_console = tk.Label(frm_console, text = 'Console Output', bg = 'black', fg = 'white', font = ('Arial', 30))
+        lbl_console.pack(anchor = 'center', padx = 50)
 
         self.grid_propagate(0)
 
