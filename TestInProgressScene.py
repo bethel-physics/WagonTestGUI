938cd150a1d17bf9fef9c337119c6e3cd3f7109f
akirzeder <104333863+akirzeder@users.noreply.github.com>
2022-06-06 08:47:10 -0500

    Fleshed Out Graphics in TestInProgressScene

--- TestInProgressScene.py
+++ TestInProgressScene.py
@@ -2,9 +2,9 @@ import tkinter as tk
 from tkinter import ttk
 
 # Creating the frame itself
-class InProgressScene1(tk.Frame):
+class TestInProgressScene(tk.Frame):
     def __init__(self, parent, master_window):
-        super().__init__(master_window)
+        super().__init__(master_window, width=700, height=500, background='purple')
 
         # Creating grid frame to lay widgets onto
         frm_main = tk.Frame(self, width = 200, height = 200)
@@ -15,7 +15,15 @@ class InProgressScene1(tk.Frame):
         lbl_title.pack(padx=200, pady=200)
 
         # Create a progress bar of some kind
-        prgbar_progress = ttk.ProgressBar(frm_main)
+        prgbar_progress = ttk.Progressbar(frm_main, orient = 'horizontal', mode = 'indeterminate', length = 280)
         prgbar_progress.pack()
+
+        # A Button to start the progress bar
+        btn_start = ttk.Button(frm_main, text='Start', command=prgbar_progress.start)
+        btn_start.pack()
+
+        # A Button To Stop the Progress Bar and Progress (Temporary until we link to actual progress)
+        btn_stop = ttk.Button(frm_main, text='Stop', command=prgbar_progress.stop)
+        btn_stop.pack()
 
         self.grid_propagate(0)
