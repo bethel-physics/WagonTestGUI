0b9ee1304538c3a00deec17321eca89ebc32d982
akirzeder <104333863+akirzeder@users.noreply.github.com>
2022-06-10 11:40:25 -0500

    Added Test Stand Variable

--- PythonFiles/DataHolder.py
+++ PythonFiles/DataHolder.py
@@ -5,6 +5,7 @@ class DataHolder():
     # List of the variables being held by data holder
     def __init__(self):
         self.user_ID = ""                 # Tester's Name
+        self.test_stand = ""              # Test stand for the unit
         self.current_serial_ID = 0        # Unit Serial Number
         self.test1_completed = False      # Whether the test has been completed
         self.test2_completed = False      # Whether the test has been completed
@@ -25,20 +26,23 @@ class DataHolder():
 
     # Prints all the variable values inside data_holder
     def print(self):
-        print("self.user_ID: ", self.user_ID)
-        print("self.current_serial_ID: ", self.current_serial_ID)
-        print("self.test1_completed: ", self.test1_completed)
-        print("self.test2_completed: ", self.test2_completed)
-        print("self.test3_completed: ", self.test3_completed)
-        print("self.test4_completed: ", self.test4_completed)
-        print("self.test1_pass: ", self.test1_pass)
-        print("self.test2_pass: ", self.test2_pass)
-        print("self.test3_pass: ", self.test3_pass)
-        print("self.test4_pass: ", self.test4_pass)
+        print("user_ID: ", self.user_ID)
+        print("test_stand: ", self.test_stand)
+        print("current_serial_ID: ", self.current_serial_ID)
+        print("test1_completed: ", self.test1_completed)
+        print("test2_completed: ", self.test2_completed)
+        print("test3_completed: ", self.test3_completed)
+        print("test4_completed: ", self.test4_completed)
+        print("test1_pass: ", self.test1_pass)
+        print("test2_pass: ", self.test2_pass)
+        print("test3_pass: ", self.test3_pass)
+        print("test4_pass: ", self.test4_pass)
 
     # Gets all the variables from data_holder
     def get(self):
-        return (self.user_ID + 
+        return (
+                self.user_ID + 
+                self.test_stand +
                 str(self.current_serial_ID) +
                 str(self.test1_completed) +
                 str(self.test2_completed) +
