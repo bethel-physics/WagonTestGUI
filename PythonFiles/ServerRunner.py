2f40eafcab2943bc8be326608e9f4d911229b830
akirzeder <104333863+akirzeder@users.noreply.github.com>
2022-06-16 11:54:28 -0500

    Made the Client into a class to be callable

---
+++ PythonFiles/ServerRunner.py
@@ -0,0 +1,9 @@
+from utils.clientZMQ import ClientZMQ
+
+# Creates a main function to initialize the GUI
+def main():
+    client1 = ClientZMQ(b"Test2")
+    
+
+if __name__ == "__main__":
+    main()
--- PythonFiles/utils/clientZMQ.py
+++ PythonFiles/utils/clientZMQ.py
@@ -7,19 +7,27 @@
 # Importing necessary modules
 import zmq
 
-context = zmq.Context()
+# Making the Client Server a class
+class ClientZMQ():
+    def __init__(self, desired_test):
+        
+        self.desired_test = desired_test
+        
+        
+        context = zmq.Context()
 
-# Creates a socket to talk to the server
-print("Connecting to the Hello World server...")
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5555")
+        # Creates a socket to talk to the server
+        print("Connecting to the Hello World server...")
+        socket = context.socket(zmq.REQ)
+        socket.connect("tcp://localhost:5555")
 
 
-# Does 10 requests, waiting for a response each time
-for request in range(10):
-    print("Sending request %s ..." % request)
-    socket.send(b"Test3")
+        # Does 10 requests, waiting for a response each time
+        for request in range(10):
+            print("Sending request %s ..." % request)
 
-    # Get the reply
-    message = socket.recv()
-    print("Received reply %s [ %s ]" % (request, message))
+            socket.send(self.desired_test)
+
+            # Get the reply
+            message = socket.recv()
+            print("Received reply %s [ %s ]" % (request, message))
