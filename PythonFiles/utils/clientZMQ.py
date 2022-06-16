ca156b935dd7ad6ebb82af631d53ad7302d79e9d
akirzeder <104333863+akirzeder@users.noreply.github.com>
2022-06-16 10:54:14 -0500

    Added Test zmq Client

---
+++ PythonFiles/utils/clientZMQ.py
@@ -0,0 +1,23 @@
+# Currently a test client server to make sure this works correctly
+#
+################################
+
+# Importing necessary modules
+import zmq
+
+context = zmq.Context()
+
+# Creates a socket to talk to the server
+print("Connecting to the Hello World server...")
+socket = context.socket(zmq.REQ)
+socket.connect("tcp:localhost:5555")
+
+
+# Does 10 requests, waiting for a response each time
+for request in range(10):
+    print("Sending request %s ..." % request)
+    socket.send(b"Hello")
+
+    # Get the reply
+    message = socket.recv()
+    print("Received reply %s [ %s ]" % (request, message))
