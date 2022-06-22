8e5bc6db2407b30b5970fbfedd7554bf837e3aa7
Garrett Schindler <garrettschindler17@gmail.com>
2022-06-22 09:52:15 -0500

    Create ServerRunner.py

---
+++ PythonFiles/utils/ServerRunner.py
@@ -0,0 +1,7 @@
+
+# The following string stores all of the prints statements that the server
+# needs to send to the client
+prints = ""
+
+rep_server = REPServer(prints)
+pub_server = PUBServer(prints)
