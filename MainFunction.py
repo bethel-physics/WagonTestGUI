8f652bb7f30ac56130903ac88a286fcadbacd944
akirzeder <104333863+akirzeder@users.noreply.github.com>
2022-06-24 13:52:21 -0500

    Added Example Code for testing what GUI you would like to run

--- MainFunction.py
+++ MainFunction.py
@@ -1,7 +1,7 @@
 # Importing necessary modules
 from audioop import mul
 import multiprocessing as mp
-import time
+import socket
 from numpy import multiply
 # Imports the GUIWindow
 from PythonFiles.GUIWindow import GUIWindow
@@ -13,19 +13,12 @@ from PythonFiles.utils.SUBClient import SUBClient
 def task_GUI(conn, queue):
     # creates the main_window as an instantiation of GUIWindow
     main_window = GUIWindow(conn, queue)
-    # print("object sent")
 
 # Creates a task of creating the SUBClient
 def task_SUBClient(conn, queue):
     # Creates the SUBSCRIBE Socket Client
     sub_client = SUBClient(conn, queue)
 
-# def task_Console(conn):
-#     print("console code started")
-#     time.sleep(2)
-#     object = conn.recv()
-#     print("object received")
-#     console = OutputConsole(conn, object)
 
 def run():    
     # Creates a Pipe for the SUBClient to talk to the GUI Window
@@ -36,13 +29,11 @@ def run():
     # Turns creating the GUI and creating the SUBClient tasks into processes
     process_GUI = mp.Process(target = task_GUI, args=(conn_GUI, queue,))
     process_SUBClient = mp.Process(target = task_SUBClient, args = (conn_SUB, queue,))
-    # process_console = mp.Process(target = task_Console, args=(conn_GUI,))
     
 
     # Starts the processes
     process_GUI.start()
     process_SUBClient.start()
-    # process_console.start()
 
     # Should hold the code at this line until the GUI process ends
     process_GUI.join()
@@ -53,12 +44,41 @@ def run():
     except:
         print("Terminate is unnecessary.")
         pass
-    # try:
-    #     # Cleans up the Console process
-    #     # process_console.terminate()
-    # except:
-    #     print("Terminate is unnecessary.")
-    #     pass
+
 
 if __name__ == "__main__":
+    print(socket.gethostname())
+    ###### Example code to branch between the different GUIS #####
+    # visual_GUI_computers = [
+    # computer_1,
+    # computer_2,
+    # etc.
+    # ]
+    # wagon_GUI_computers = [
+    # computer_3,
+    # computer_4,
+    # etc.
+    # ]
+    # engine_GUI_computers = [
+    # computer_5,
+    # computer_6,
+    # etc.
+    # ]
+    # current_computer = socket.gethostname()
+    # for computer in visual_GUI_computers:
+    #    if current_computer == computer:
+    #        run_visual_GUI()
+    #    else:
+    #        pass
+    # for computer in wagon_GUI_computers:
+    #    if current_computer == computer:
+    #        run_wagon_GUI()
+    #    else:
+    #        pass
+    # for computer in engine_GUI_computers:
+    #    if current_computer == computer:
+    #        run_engine_GUI()
+    #    else:
+    #        pass
+    ###### END EXAMPLE CODE ######
     run()
