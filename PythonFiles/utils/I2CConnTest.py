7577f7194dd76c4d1f920e2e4ec5558111b740f0
akirzeder <104333863+akirzeder@users.noreply.github.com>
2022-06-24 10:53:25 -0500

    Updated All Tests to deliver Done and JSON

--- PythonFiles/utils/I2CConnTest.py
+++ PythonFiles/utils/I2CConnTest.py
@@ -1,14 +1,18 @@
-import time
+import time, json
 
 
 class I2CConnTest():
 
     def __init__(self, conn):
-        time.sleep(1)
-        conn.send("One")
-        time.sleep(2)
-        conn.send("Two")
-        time.sleep(2)
-        conn.send("Three")
-        time.sleep(2)
+        for i in range(10):
+            conn.send("Run:"+ str(i))
+            time.sleep(0.25)
+
         conn.send("Done.")
+        time.sleep(0.25)
+        # Test code to ensure json/text sending is working correctly
+        current_JSON_file = open("./PythonFiles/JSONFiles/testingJSON.JSON")
+        current_JSON_data = json.load(current_JSON_file)
+        json_string = json.dumps(current_JSON_data)
+        print(json_string)
+        conn.send(json_string)
