dd9ac4f9fea4c5fb55250f332840c71f39b5e012
akirzeder <104333863+akirzeder@users.noreply.github.com>
2022-06-29 15:41:01 -0500

    Fleshed Out TestScripts

--- PythonFiles/utils/StressScript.py
+++ PythonFiles/utils/StressScript.py
@@ -5,10 +5,13 @@ from random import randint
 
 class StressScript():
     def __init__(self, conn, serial = "", tester = ""):
-        test_length = randint(0,1000)
+        test_length = randint(500,1500)
         i = 0
         while i < test_length:
             S = 10  
             ran = ''.join(random.choices(string.ascii_uppercase + string.digits, k = S))
             conn.send(ran)
             i += 1
+        conn.send("Done.")
+        conn.send("Run completed")
+        
